d654bd9d0fecff99130e6ccb4fb2491c62c8d5e5
Ivan <pinjamar@gmail.com>
2024-02-13 12:51:55 +0100

    added a header and some basic components

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
     <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>CRYPTO</title>
-        <link rel="icon" href="/favicon.ico"></link>
+        <link rel="icon" href="../public/favicon.ico"></link>
       </Head>
       <div className="grid grid-cols-1 ls:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
