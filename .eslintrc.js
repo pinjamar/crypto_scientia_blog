6875559b1b20c301217381f4128f1b62e2bd6671
Ivan <pinjamar@gmail.com>
2024-05-07 13:39:50 +0200

    linter fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
