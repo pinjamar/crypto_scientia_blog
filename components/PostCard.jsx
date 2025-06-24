a8640e05e9378b66c96940b5282c1c69d666d466
Ivan <pinjamar@gmail.com>
2025-06-24 21:38:16 +0200

    removing excerpt from mobileview

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -55,7 +55,7 @@ const PostCard = ({ post }) => (
         </span>
       </div>
     </div>
-    <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+    <p className="hidden sm:block text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
       {post.excerpt}
     </p>
     <div className="text-center">
